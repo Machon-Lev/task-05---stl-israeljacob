0eb8ca3278c2d19bc0f529417d0d990857ea2965
Yisrael jacob <yisraeljakob@gmail.com>
2023-06-25 21:02:15 +0300

    Comments and readme

--- task6__israel_jacob/src/Cities.cpp
+++ task6__israel_jacob/src/Cities.cpp
@@ -1,16 +1,17 @@
-#include"Cities.h"
-#include<iostream>
-#include<fstream>
+#pragma once
+#include "Cities.h"
+#include <iostream>
+#include <fstream>
 
 #include <cstdlib>
 
-
+// Constructor
 Cities::Cities() {
 	cities = std::map<std::string, Location>();
 }
 
-std::string Cities::checkFile(std::ifstream &inputFile){
-
+// Helper method to check file validity and return the name of the first city
+std::string Cities::checkFile(std::ifstream& inputFile) {
 	if (!inputFile.is_open()) {
 		throw std::exception("Unable to open file.");
 	}
@@ -24,8 +25,8 @@ std::string Cities::checkFile(std::ifstream &inputFile){
 	return name;
 }
 
-void Cities::addFromFile(std::string path)
-{
+// Method to add cities from a file
+void Cities::addFromFile(std::string path) {
 	std::ifstream inputFile(path);
 
 	std::string name;
@@ -49,53 +50,52 @@ void Cities::addFromFile(std::string path)
 			inputFile.close();
 			throw std::exception("The information is invalid");
 		}
-		
+
 	} while (std::getline(inputFile, name));
 	inputFile.close();
 }
 
-bool Cities::exsists(std::string name)
-{
+// Method to check if a city exists
+bool Cities::exists(std::string name) {
 	auto i = cities.find(name);
 	return cities.find(name) != cities.end();
 }
 
-std::vector<std::pair<std::string, Location>> Cities::nearCities(std::string name, double radius, int norm)
-{
+// Method to find cities near a specified city within a given radius using a specified distance norm
+std::vector<std::pair<std::string, Location>> Cities::nearCities(std::string name, double radius, int norm) {
 	auto cityIterator = cities.find(name);
 
 	double x = cityIterator->second.getX();
 	double y = cityIterator->second.getY();
 	std::vector<std::pair<std::string, Location>> nearCities = distanceBySquare(radius, x, y);
 
-	if(norm == 0)
+	if (norm == 0)
 		return normalEuclideanDistance(nearCities, radius, x, y);
-	else if(norm == 1)
+	else if (norm == 1)
 		return infinityNorm(nearCities, radius, x, y);
 	else
 		return manhattanDistance(nearCities, radius, x, y);
 }
 
-std::vector<std::pair<std::string, Location>> Cities::distanceBySquare(double distance, double x, double y)
-{
+// Helper method to filter cities within a square distance from a given location
+std::vector<std::pair<std::string, Location>> Cities::distanceBySquare(double distance, double x, double y) {
 	std::vector<std::pair<std::string, Location>> vCities;
 	std::copy_if(cities.begin(), cities.end(), std::back_inserter(vCities),
-		[x, y, distance](const std::pair<const std::string, const Location>& pair)
-		{
-			double dx =  std::abs(pair.second.getX() - x);
-			double dy = std::abs(pair.second.getY() - y);
-			return dx <= distance && dy <= distance && (dx != 0 || dy != 0);
+		[x, y, distance](const std::pair<const std::string, const Location>& pair) {
+			double dx = std::abs(pair.second.getX() - x);
+	double dy = std::abs(pair.second.getY() - y);
+	return dx <= distance && dy <= distance && (dx != 0 || dy != 0);
 		});
 	return vCities;
 }
 
-std::vector<std::pair<std::string, Location>> Cities::normalEuclideanDistance(std::vector<std::pair<std::string, Location>> vCities, double radius, double x, double y)
-{
+// Helper method to filter cities within a radius using the Euclidean distance
+std::vector<std::pair<std::string, Location>> Cities::normalEuclideanDistance(std::vector<std::pair<std::string, Location>> vCities, double radius, double x, double y) {
 	vCities.erase(std::remove_if(vCities.begin(), vCities.end(),
 		[x, y, radius](std::pair<std::string, Location> pair) {
 			double dx = pair.second.getX() - x;
-			double dy = pair.second.getY() - y;
-			return (dx * dx + dy * dy) <= radius * radius; }), vCities.end());
+	double dy = pair.second.getY() - y;
+	return (dx * dx + dy * dy) >= radius * radius; }), vCities.end());
 	std::sort(vCities.begin(), vCities.end(),
 		[x, y](std::pair<std::string, Location> pair1, std::pair<std::string, Location> pair2) {
 			double dx1 = pair1.second.getX() - x;
@@ -106,40 +106,45 @@ std::vector<std::pair<std::string, Location>> Cities::normalEuclideanDistance(st
 	return vCities;
 }
 
-std::vector<std::pair<std::string, Location>> Cities::infinityNorm(std::vector<std::pair<std::string, Location>> vCities, double radius, double x, double y)
-{
+// Helper method to filter cities within a radius using the infinity norm
+std::vector<std::pair<std::string, Location>> Cities::infinityNorm(std::vector<std::pair<std::string, Location>> vCities, double radius, double x, double y) {
 	vCities.erase(std::remove_if(vCities.begin(), vCities.end(),
 		[x, y, radius](std::pair<std::string, Location> pair) {
 			double dx = std::abs(pair.second.getX() - x);
-			double dy = std::abs(pair.second.getY() - y);
-			return std::max(dx, dy) <= radius; }), vCities.end());
+	double dy = std::abs(pair.second.getY() - y);
+	return std::max(dx, dy) >= radius;
+		}), vCities.end());
 	std::sort(vCities.begin(), vCities.end(),
 		[x, y](std::pair<std::string, Location> pair1, std::pair<std::string, Location> pair2) {
 			double dx1 = std::abs(pair1.second.getX() - x);
-			double dy1 = std::abs(pair1.second.getY() - y);
-			double dx2 = std::abs(pair2.second.getX() - x);
-			double dy2 = std::abs(pair2.second.getY() - y);
-			return std::max(dx1, dy1) <= std::max(dx2, dy2); });
+	double dy1 = std::abs(pair1.second.getY() - y);
+	double dx2 = std::abs(pair2.second.getX() - x);
+	double dy2 = std::abs(pair2.second.getY() - y);
+	return std::max(dx1, dy1) <= std::max(dx2, dy2);
+		});
 	return vCities;
 }
 
-std::vector<std::pair<std::string, Location>> Cities::manhattanDistance(std::vector<std::pair<std::string, Location>> vCities, double radius, double x, double y)
-{
+// Helper method to filter cities within a radius using the Manhattan distance
+std::vector<std::pair<std::string, Location>> Cities::manhattanDistance(std::vector<std::pair<std::string, Location>> vCities, double radius, double x, double y) {
 	vCities.erase(std::remove_if(vCities.begin(), vCities.end(),
 		[x, y, radius](std::pair<std::string, Location> pair) {
 			double dx = std::abs(pair.second.getX() - x);
-			double dy = std::abs(pair.second.getY() - y);
-			return dx + dy <= radius; }), vCities.end());
-	std::sort(vCities.begin(), vCities.end(), 
-		[x,y](std::pair<std::string, Location> pair1, std::pair<std::string, Location> pair2) {
+	double dy = std::abs(pair.second.getY() - y);
+	return dx + dy >= radius;
+		}), vCities.end());
+	std::sort(vCities.begin(), vCities.end(),
+		[x, y](std::pair<std::string, Location> pair1, std::pair<std::string, Location> pair2) {
 			double dx1 = std::abs(pair1.second.getX() - x);
-			double dy1 = std::abs(pair1.second.getY() - y);
-			double dx2 = std::abs(pair2.second.getX() - x);
-			double dy2 = std::abs(pair2.second.getY() - y);
-			return dx1 + dy1 <= dx2 + dy2;});
+	double dy1 = std::abs(pair1.second.getY() - y);
+	double dx2 = std::abs(pair2.second.getX() - x);
+	double dy2 = std::abs(pair2.second.getY() - y);
+	return dx1 + dy1 <= dx2 + dy2;
+		});
 	return vCities;
 }
 
+// Method to determine the number of cities located north of a given city
 int Cities::northOfTheCity(std::vector<std::pair<std::string, Location>> vCities, std::string name)
 {
 	double y = cities.find(name)->second.getY();
--- task6__israel_jacob/src/main.cpp
+++ task6__israel_jacob/src/main.cpp
@@ -1,33 +1,54 @@
 #include"Cities.h"
 #include<iostream>
+#include<vector>
+#include <iostream>
 
-std::string getName(Cities cities); 
+std::string getName(Cities cities);
 double getRadius();
 int getNorm();
 
 int main()
 {
 	Cities cities = Cities();
+	// Create an instance of the Cities class
+
 	try {
 		cities.addFromFile("data.txt");
+		// Attempt to load city data from the "data.txt" file
 	}
-	catch (const std::exception& e){
+	catch (const std::exception& e) {
 		std::cout << e.what();
 		return 0;
 	}
+
 	std::vector<std::pair<std::string, Location>> nearCities;
+	// Vector to store the nearby cities
 	int numOfCitiesInNorth;
+	// Variable to store the number of cities to the north
 	std::string name;
 	double radius;
 	int norm;
-	
+
 	name = getName(cities);
+	// Get the name of the selected city from the user
 	while (name != "0")
 	{
+		try {
+		// Get the desired search radius from the user
 		radius = getRadius();
+
+		// Get the desired norm from the user
 		norm = getNorm();
+		}
+		catch (std::exception& e)
+		{
+			std::cout << e.what() << std::endl;
+			return 0;
+		}
 		nearCities = cities.nearCities(name, radius, norm);
+		// Retrieve the cities within the specified radius using the specified norm
 		numOfCitiesInNorth = cities.northOfTheCity(nearCities, name);
+		// Count the number of cities to the north of the selected city
 		std::cout << "Search result:" << std::endl << nearCities.size()
 			<< " city / cities found in the given radius." << std::endl
 			<< numOfCitiesInNorth << " cities are to the north of the selected city."
@@ -37,10 +58,15 @@ int main()
 			[](const std::pair<std::string, Location>& pair) {
 				return pair.first; // Extract the string from the pair
 			});
+		// Extract only the city names from the pairs and store them in the 'names' vector
 		std::copy(names.begin(), names.end(), std::ostream_iterator<std::string>(std::cout, "\n"));
+		// Print the city names to the console
 		std::cin.ignore();
+		// Ignore any remaining input in the buffer
 		name = getName(cities);
+		// Get the name of the next selected city from the user
 	}
+
 	std::cout << "bye" << std::endl;
 	return 0;
 }
@@ -49,27 +75,34 @@ int main()
 std::string getName(Cities cities) {
 	std::string name;
 
-		std::cout << "Please enter selected city name (with line break after it):" << std::endl;
+	std::cout << "Please enter selected city name (with line break after it):" << std::endl;
 	std::getline(std::cin, name);
-	while (!(cities.exsists(name)) && name != "0") {
-		std::cout << "ERROR: The city dos not exsists" << std::endl;
+	// Prompt the user to enter the name of the selected city
+
+	while (!(cities.exists(name)) && name != "0") {
+		std::cout << "ERROR: The city does not exist" << std::endl;
 		std::cout << "Please enter selected city name (with line break after it):" << std::endl;
 		std::getline(std::cin, name);
+		// Continue prompting the user until a valid city name is entered
 	}
 	return name;
+	// Return the valid city name entered by the user
 }
 
+
 double getRadius() {
-	double radius;
+	std::string radius;
+	double dradius;
 	std::cout << "Please enter the wanted radius:" << std::endl;
 	try {
 		std::cin >> radius;
+		dradius = stod(radius);
 	}
 	catch (std::exception& e) {
 		throw std::exception("You must not enter characters instead of numbers");
 	}
 
-	while (radius < 0) {
+	while (dradius < 0) {
 		std::cout << "ERROR: The radius can't be negative" << std::endl;
 		std::cout << "Please enter the wanted radius:" << std::endl;
 		try {
@@ -79,11 +112,12 @@ double getRadius() {
 			throw std::exception("You must not enter characters instead of numbers");
 		}
 	}
-	return radius;
+	return dradius; // Return the valid radius entered by the user
 }
 
 int getNorm() {
-	int norm;
+	std::string norm;
+	int inorm;
 	std::cout << "Please enter the wanted norm"
 		<< "(0 - L2, Euclidean distance, "
 		<< "1 - Linf, Chebyshev distance, "
@@ -91,12 +125,13 @@ int getNorm() {
 		<< std::endl;
 	try {
 		std::cin >> norm;
+		inorm = stod(norm);
 	}
 	catch (std::exception& e) {
 		throw std::exception("You must not enter characters instead of numbers");
 	}
 
-	while (norm < 0 || norm > 2) {
+	while (inorm < 0 || inorm > 2) {
 		std::cout << "ERROR: The norm has to be an integer between 0-2" << std::endl;
 		std::cout << "Please enter the wanted norm"
 			<< "(0 – L2, Euclidean distance, "
@@ -104,11 +139,11 @@ int getNorm() {
 			<< "2 – L1, Manhattan distance) : "
 			<< std::endl;
 		try {
-			std::cin >> norm;
+			std::cin >> inorm;
 		}
 		catch (std::exception& e) {
 			throw std::exception("You must not enter characters instead of numbers");
 		}
 	}
-	return norm;
+	return inorm; // Return the valid norm entered by the user
 }
